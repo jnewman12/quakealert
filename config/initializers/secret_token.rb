a3d5a986e3de90cbdedeccc9b199cfb92beef0dc
James Newman <jamesmnewman1@gmail.com>
2014-05-06 23:04:03 -0700

    setting up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quakealert::Application.config.secret_key_base = 'b5aed736065e1186affd6646747ae53afffbf6cee831d7f036b17c68818f14bf914d36b359cd5c476f85c5a4069da29eb313a0599fe8f3ebff2a2a8dc4d393ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quakealert::Application.config.session_store :cookie_store, key: '_quakealert_session'
