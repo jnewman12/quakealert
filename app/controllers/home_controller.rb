6f7b1ac585d2daf8e86a80a3a923e6b2f5f508c0
James Newman <jamesmnewman1@gmail.com>
2014-05-28 22:30:04 -0700

    messy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,7 @@ class HomeController < ApplicationController
 
 	def welcome
 	end
+
+	def newer
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Quakealert::Application.routes.draw do
  root to: "home#index"
 
  get "/new" => "home#welcome" 
+
+ get "/newer" => "home#newer"
 end
