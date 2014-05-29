bb571e798359fc8ef6bfc9785275508f10142501
James Newman <jamesmnewman1@gmail.com>
2014-05-28 21:08:22 -0700

    coming along

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
 
 	def index
 	end
+
+	def welcome
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Quakealert::Application.routes.draw do
  root to: "home#index"
+
+ get "/new" => "home#welcome" 
 end
